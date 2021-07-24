023696bacdeaddc82108fe1b85eecc134ae68be4
eunyoungko <eunyoung4444@gmail.com>
2021-07-24 21:14:26 +0200

    update boxes along with curr image no

--- front/src/views/Instruction.vue
+++ front/src/views/Instruction.vue
@@ -96,7 +96,8 @@ export default {
         function(self, res){ // eslint-disable-line no-unused-vars
           console.log(res.data)
           self.$store.commit('set_assigned_images', res.data.assigned_images);
-          self.$store.commit('set_start_image_no', res.data.assigned_images[1]);
+          self.$store.commit('set_start_image_no', res.data.assigned_images[0]);
+          self.$store.commit('set_curr_image', 1);
           self.$router.push('annotation/'+res.data.doctype);
         })
     }
