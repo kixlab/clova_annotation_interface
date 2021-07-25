b8d20d0d2fb553cef6240dbc6bbc6a36c0a797a5
eunyoungko <eunyoung4444@gmail.com>
2021-07-25 22:11:06 +0200

    update json_url accordingly

--- front/src/components/DeferredAnnotation.vue
+++ front/src/components/DeferredAnnotation.vue
@@ -36,9 +36,9 @@
                     <b>{{subcat.subcat}}</b>: {{subcat.description}}
                     <span v-if="subcat.subcat!='n/a'" class='conf-btn'>
                     <v-btn x-small outlined color="success" style='margin-right:1px;' v-on:click.stop="annotate(subcat, 1)">Exactly</v-btn>
-                        <v-tooltip right> 
+                        <v-tooltip v-model="subcat.show" right> 
                         <template v-slot:activator="{ on }">
-                          <v-btn x-small outlined color="warning" v-on="{on}">
+                          <v-btn x-small outlined color="warning" @click="subcat.show = true">
                             Can be
                           </v-btn>
                         </template>
