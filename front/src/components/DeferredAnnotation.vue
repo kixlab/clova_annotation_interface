a6ee831c0fda6d4b3f4fdbe524d80df374e2b903
eunyoungko <eunyoung4444@gmail.com>
2021-07-28 00:27:13 +0200

    mute console

--- front/src/components/DeferredAnnotation.vue
+++ front/src/components/DeferredAnnotation.vue
@@ -176,8 +176,6 @@ export default {
             }
         }
 
-        console.log(item, suggestion)
-
         //this.$helpers.server_log(this, 'CL', group.map((i) => {return i.box_id}), label)
         this.updateImageBoxes(this.image_box)
 
