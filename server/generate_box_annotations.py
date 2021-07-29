efba0f6fa07be8da94648b32c971fc3f612171ef
hoonhan <hanuright@gmail.com>
2021-07-29 05:43:40 +0000

    dash setup

---
+++ server/generate_box_annotations.py
@@ -0,0 +1,39 @@
+import csv
+from api.models import *
+from dashboard.models import * 
+
+# python manage.py shell
+# exec(open('generate_box_annotations.py').read())
+
+
+BoxAnnotation.objects.all().delete()
+
+annotations=Annotation.objects.filter(is_alive=True)
+
+
+
+for annotation in annotations: 
+    user=annotation.user
+    document=annotation.document
+    subcat=annotation.subcat
+    cat=annotation.cat
+    
+
+    boxes_id=annotation.boxes_id.replace(',', ' ').replace('[', ' ').replace(']', ' ').split()
+    if(annotation.confidence):
+        annot_type='exactly'
+        suggested_subcat=''
+    else:
+        suggestion=SelectedSuggestion.objects.filter(annotation=annotation)
+        suggested_subcat=''
+        if(len(suggestion)>0):
+            suggsted_subcat=suggestion[0].suggestion.suggested_subcat 
+
+        if(annotation.subcat.subcat_text=='n/a'):
+            annot_type='na'
+        else: 
+            annot_type='closeto'
+    for box_id in boxes_id:
+        newBox=BoxAnnotation(user=user, document=document, subcat=subcat, cat=cat, annotation=annotation, annot_type=annot_type, suggested_subcat=suggested_subcat, 
+        box_id=box_id)
+        newBox.save()
---
+++ server/generate_raw_annotations.py
@@ -0,0 +1,27 @@
+import csv
+from api.models import *
+from dashboard.models import * 
+
+# python manage.py shell
+# exec(open('generate_raw_annotations.py').read())
+
+max_docno=210
+
+for doc_no in range(max_docno):
+    document=Document.objects.get(doctype__doctype='receipt', doc_no=doc_no)
+    thisBoxAnnots=BoxAnnotation.objects.filter(document=document)
+    cand_box_ids=list(set([box.box_id for box in thisBoxAnnots]))
+    for box_id in cand_box_ids: 
+        currBoxAnnots=thisBoxAnnots.filter(box_id=box_id)
+        exactBoxAnnots=currBoxAnnots.filter(annot_type='exactly')
+        if(len(currBoxAnnots)==len(exactBoxAnnots)):
+            has_suggestion=False
+        else:
+            has_suggestion=True
+        subcats=[ba.annotation.subcat for ba in currBoxAnnots]
+        finsubcat=subcats[0]
+        for subcat in subcats:
+            if(subcats.count(subcat)>1):
+                finsubcat=subcat
+        newRawAnnot=RawAnnotation(document=document, subcat=finsubcat, box_id=box_id, has_suggestion=has_suggestion)
+        newRawAnnot.save()
