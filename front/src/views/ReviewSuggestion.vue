120c16b132987bb919ff1eaf0f451b4dcfe2644b
eunyoungko <eunyoung4444@gmail.com>
2021-08-04 13:28:47 +0200

    get others suggestion on same subbcat

--- front/src/views/ReviewSuggestion.vue
+++ front/src/views/ReviewSuggestion.vue
@@ -104,7 +104,7 @@ export default {
       }
     }).then(function(res){
         console.log(res);
-        self.issue_list=res.data;
+        self.issue_list=res.data.suggestions;
     })
         // get annotation 단위 --> image id, box id(s), suggestion
     },
