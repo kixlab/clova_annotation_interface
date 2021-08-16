847f8117725a97ac2efb13628f6b82f78a50270b
eunyoungko <eunyoung4444@gmail.com>
2021-08-16 13:03:24 +0900

    only shows unreviewed issues

--- front/src/views/ReviewSuggestion.vue
+++ front/src/views/ReviewSuggestion.vue
@@ -19,14 +19,14 @@
                             <!--<h3>Suggestions</h3>-->
                             <div>Please click on one of your suggestions!</div>
                             <h4 style="text-align: left; margin-top: 10px">Close To Suggestions</h4>
-                            <div v-for="(v, idx) in issues_with_suggestions.filter(v => v.suggestion_subcat !== 'n/a')" :key="'closeto-' + idx" style="overflow-y: scroll">
+                            <div v-for="(v, idx) in unreviewed_issues.filter(v => v.suggestion_subcat !== 'n/a')" :key="'closeto-' + idx" style="overflow-y: scroll">
                                 <v-btn depressed :outlined="v.suggestion_pk !== sel_issue.suggestion_pk" color="warning" small style="margin: 5px" @click="clickCloseto(v)"> 
                                     {{v.suggestion_cat}}-{{v.suggestion_subcat}} ({{v.suggestion_text}})
                                 </v-btn>
                                 x {{v.n_issues}}
                             </div>
                             <h4 style="text-align: left; margin-top: 10px">N/A Suggestions</h4>
-                            <div v-for="(v, idx) in issues_with_suggestions.filter(v => v.suggestion_subcat === 'n/a')" :key="'n/a-' + idx" style="overflow-y: scroll">
+                            <div v-for="(v, idx) in unreviewed_issues.filter(v => v.suggestion_subcat === 'n/a')" :key="'n/a-' + idx" style="overflow-y: scroll">
                                 <v-btn depressed :outlined="v.suggestion_pk !== sel_issue.suggestion_pk" color="error" small style="margin: 5px" @click="clickNa(v)"> 
                                     {{v.suggestion_cat}}-{{v.suggestion_subcat}} ({{v.suggestion_text}})
                                 </v-btn>
