0e6a8333aea3594c19f072660db327feb48d5ca7
eunyoungko <eunyoung4444@gmail.com>
2021-08-25 00:29:28 +0900

    dashboard models

--- server/dashboard/models.py
+++ server/dashboard/models.py
@@ -14,19 +14,12 @@ from api.models import *
 
 class TargetAnnotation(models.Model):
     expert=models.ForeignKey(User, on_delete=models.CASCADE, related_name='expert_users')
-    user=models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_users')
-    document=models.ForeignKey(Document, on_delete=models.SET_NULL, null=True)
-    subcat=models.ForeignKey(InitSubCat, on_delete=models.CASCADE)
-    cat=models.ForeignKey(InitCat, on_delete=models.CASCADE)
-    annotation=models.ForeignKey(Annotation, on_delete=models.SET_NULL, null=True, blank=True)
-
-    annot_type=models.CharField(max_length=255) #exactly, closeto, na 
-    suggested_subcat=models.CharField(max_length=255, null=True, blank=True)
-
-    box_id=models.IntegerField(default=999, null=True, blank=True)
+    doctype=models.ForeignKey(DocType, on_delete=models.CASCADE, null=True)
+    annotation=models.ForeignKey(GroupedAnnotation, on_delete=models.SET_NULL, null=True, blank=True)
     is_reviewed=models.BooleanField(default=False)
+
     def __str__(self):
-        return 'boxannot-'+self.expert.username+'-'+str(self.document)+'-'+str(self.box_id)+'-'+self.annot_type
+        return 'targetannot-'+self.expert.username
 """ 
 class TargetBoxAnnotation(models.Model):
     expert=models.ForeignKey(User, on_delete=models.CASCADE, related_name='expert_users')
@@ -44,6 +37,21 @@ class TargetBoxAnnotation(models.Model):
     def __str__(self):
         return 'boxannot-'+self.expert.username+'-'+str(self.document)+'-'+str(self.box_id)+'-'+self.annot_type
  """
+
+class FinalCat(models.Model):
+    expert=models.ForeignKey(User, on_delete=models.CASCADE)
+    doctype=models.ForeignKey(DocType, on_delete=models.CASCADE, null=True)
+    cat_text=models.CharField(max_length=255)
+    def __str__(self):
+        return str(self.expert)+'-'+str(self.cat_text)
+
+class FinalSubCat(models.Model):
+    finalcat=models.ForeignKey(FinalCat, on_delete=models.CASCADE)
+    subcat_text=models.CharField(max_length=255)
+    subcat_description=models.CharField(max_length=255)
+    def __str__(self):
+        return str(self.finalcat.expert)+'-'+str(self.subcat_text)
+
 class FinalLabel(models.Model):
     expert=models.ForeignKey(User, on_delete=models.CASCADE)
     doctype=models.ForeignKey(DocType, on_delete=models.CASCADE)
@@ -54,16 +62,23 @@ class FinalLabel(models.Model):
 
 class RevisedAnnotation(models.Model):
     expert=models.ForeignKey(User, on_delete=models.CASCADE)
-    document=models.ForeignKey(Document, on_delete=models.SET_NULL, null=True)
-    label=models.ForeignKey(FinalLabel, on_delete=models.SET_NULL, null=True)
+    annotation=models.ForeignKey(GroupedAnnotation, on_delete=models.SET_NULL, null=True)
+    finalcat=models.ForeignKey(FinalCat, on_delete=models.SET_NULL, null=True)
+    finalsubcat=models.ForeignKey(FinalSubCat, on_delete=models.SET_NULL, null=True)
+    revision_type=models.CharField(max_length=225) #na-new, na-existing, auto-mv
 
-    box_id=models.IntegerField(default=999, null=True, blank=True)
-    revision_type=models.CharField(max_length=225) #na-approve, na-new, na-existing, ct-approve, ct-new, ct-ignore, 'auto'
-    #is_alive=models.BooleanField(default=False)
 
-class RawAnnotation(models.Model):
-#    user=models.ForeignKey(User, on_delete=models.CASCADE)
+class RawBoxAnnotation(models.Model): ## same for each user 
     document=models.ForeignKey(Document, on_delete=models.SET_NULL, null=True)
-    subcat=models.ForeignKey(InitSubCat, on_delete=models.SET_NULL, null=True)
-    box_id=models.IntegerField(default=999, null=True, blank=True)
-    has_suggestion=models.BooleanField(default=False)
+    initcat=models.ForeignKey(InitCat, on_delete=models.SET_NULL, null=True)
+    initsubcat=models.ForeignKey(InitSubCat, on_delete=models.SET_NULL, null=True)
+    box_id=models.IntegerField(default=999)
+
+
+class RevisedBoxAnnotation(models.Model):
+    expert=models.ForeignKey(User, on_delete=models.CASCADE)
+    document=models.ForeignKey(Document, on_delete=models.SET_NULL, null=True)
+    finalcat=models.ForeignKey(FinalCat, on_delete=models.SET_NULL, null=True)
+    finalsubcat=models.ForeignKey(FinalSubCat, on_delete=models.SET_NULL, null=True)
+    box_id=models.IntegerField(default=999)
+    revision_type=models.CharField(max_length=225) #na-new, na-existing, auto-mv
